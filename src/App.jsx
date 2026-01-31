565b9bd9304df2c581f7b42bb159b571d9793e75
Sunetra Bar <sunetrabarofficial2025@gmail.com>
2026-01-31 10:00:15 +0530

    jsx to tsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Timeline from './components/timeline.jsx'
+import Timeline from './components/timeline'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <Timeline/>
+      <Timeline />
     </>
   )
 }
